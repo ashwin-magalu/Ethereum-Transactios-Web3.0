c6bcd92cdb03973cad821dbe3b5999dbf4b98eaa
Ashwin M A <ashwin.magalu@gmail.com>
2022-06-09 21:54:17 +0530

    add

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import transactions from "./Transactions.json";
 
 export const contractABI = transactions.abi;
-export const contractAddress = "0x45FD11DA00C9C03311353Be87Fd91d2D01a4EA49";
+export const contractAddress = "0x45FD11DA00C9C03313Be87Fd91d2D01a4EA49";
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,9 +4,9 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/Qty6XzalS4JF17q1p1z8NXYoy0ziVaX0",
+      url: "https://eth-rinkeby.alchemyapi.io/v2/Qty6XzalS41p1z8NXYoy0ziVaX0",
       accounts: [
-        "aff106d53b3a206af025a694f413b13eb9c1fbf62c28403b19cc4332164f0f4f",
+        "aff106d53b3a206a694f413b13eb9c1fbf62c28403b19cc4332164f0f4f",
       ], //  Get it from Export private key in Metamask
     },
   },
